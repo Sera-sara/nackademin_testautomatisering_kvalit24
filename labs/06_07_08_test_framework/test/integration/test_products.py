0f9593f18851e2ff56fed8f7f4e7330154b9f06c
Sara <sara.kanngard@gmail.com>
2025-09-25 13:58:29 +0200

    Update test_products.py

--- labs/06_07_08_test_framework/test/integration/test_products.py
+++ labs/06_07_08_test_framework/test/integration/test_products.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 
 
 def test_add_product_to_catalog():  # complete code
-    user_api = UserAPI("http://localhost:8000")
-    admin_api = AdminAPI("http://localhost:8000")
+    user_api = UserAPI(os.getenv("BASE_URL"))
+    admin_api = AdminAPI(os.getenv("BASE_URL"))
     username = "Admin_user"
     password = "Automation53"
     # Given I am an admin user​
@@ -24,8 +24,8 @@ def test_add_product_to_catalog():  # complete code
 
 
 def test_remove_product_from_catalog():  # complete code
-    user_api = UserAPI("http://localhost:8000")
-    admin_api = AdminAPI("http://localhost:8000")
+    user_api = UserAPI(os.getenv("BASE_URL"))
+    admin_api = AdminAPI(os.getenv("BASE_URL"))
     username = "Admin_user"
     password = "Automation53"
     # Given I am an admin user​
